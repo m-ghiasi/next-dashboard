f3bc950380b5d9295b9d4900d8d111e9466edaae
m-ghiasi <mahdiye71gh@gmail.com>
2025-03-22 01:10:32 +0430

    card component

---
+++ src/app/dashboard/cards.tsx
@@ -0,0 +1,39 @@
+import {
+  BanknotesIcon,
+  ClockIcon,
+  UserGroupIcon,
+  InboxIcon,
+} from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
+import { customers } from "../lib/placeholder";
+
+const iconMap = {
+  collected: BanknotesIcon,
+  customers: UserGroupIcon,
+  pending: ClockIcon,
+  invoices: InboxIcon,
+};
+
+export default function Card({
+  type,
+  title,
+  value,
+}: {
+  type: "collected" | "customers" | "pending" | "invoices";
+  title: string;
+  value: string | number;
+}) {
+
+    const Icon = iconMap[type]
+  return (
+    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+      <div  className="flex p-4">
+        {Icon ? <Icon className="h-5 w-5 text-gray-700"/> : null}
+        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+      </div>
+      <p className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl `}>
+        {value}
+      </p>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import Card from "./cards"
 export default function Dashboard(){
     return(
         <div className="bg-amber-400">Dashboard Page...</div>
