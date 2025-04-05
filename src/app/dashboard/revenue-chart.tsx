741f15b82fdecbb1b2f18f48ab594c563ee83286
m-ghiasi <mahdiye71gh@gmail.com>
2025-04-05 13:07:35 +0430

    revenue chart file

--- src/app/dashboard/revenue-chart.tsx
+++ src/app/dashboard/revenue-chart.tsx
@@ -2,3 +2,23 @@ import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { Revenue } from '@/app/lib/definitionsNew';
+
+
+
+export default async function  RevenueChart({revenue} : {
+    revenue : Revenue[] 
+}) {
+
+    const chartHeight = 350 ;
+
+    const {yAxisLabels , topLabel} = generateYAxis(revenue);
+
+    if (!revenue || revenue.length === 0) {
+        return  <p className='mt-4 text-gray-400'>No data available.</p>
+    }
+
+    return (
+        
+    )
+    
+}
