2fa081e62479a49c9038fdef768d5a4f8a11de9b
m-ghiasi <mahdiye71gh@gmail.com>
2025-04-13 14:30:39 +0430

    edit

--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -10,15 +10,20 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const pathName = usePathname();
     const {replace} = useRouter();
 
-    const handleSearch = useDebouncedCallback ((term:string) {
-            console.log(term)
-            const params = new URLSearchParams(searchParams)
-            if (term){
-                params.set('query', term)
-            } else {
-                params.delete('query')
-            } replace (`${pathName}?${params.toString()}`);
-    },300)
+    const handleSearch = useDebouncedCallback((term: string) => {
+      console.log(`Searching... ${term}`);
+  
+      const params = new URLSearchParams(searchParams);
+  
+      params.set('page', '1');
+  
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathName}?${params??""}`);
+    }, 300);
 
 
   return (
